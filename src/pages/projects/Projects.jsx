31159ae891b48f17f98ff3050b9e935ef1413bd9
chimbo87 <archfordnhanga@gmail.com>
2024-03-13 15:15:33 +0200

    commit all

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import "./Projects.css";
 import blogpic02 from "../../assets/construction.jpg";
+import project1 from "../../assets/homebanner.jpeg";
+import project2 from "../../assets/about.jpg";
+import { Image } from "antd";
+import Footer from "../../components/footer/Footer";
 
 function Projects() {
   const [activeButton, setActiveButton] = useState(null);
@@ -82,26 +86,148 @@ function Projects() {
         <div class="row">
           <div class="col-lg-6 col-md-4">
             <div id="projectCard">
-              <div id="projectCardImg">
-                <img src={blogpic02} />
+              <div id="projectCardImgB">
+                <Image
+                  src={blogpic02}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
               </div>
               <div id="projectCardImg">
-                <img src={blogpic02} />
+                <Image
+                  src={project1}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
               </div>
             </div>
           </div>
           <div class="col-lg-6 col-md-4">
             <div id="projectCard">
-              <div id="projectCardImg">
-                <img src={blogpic02} />
+              <div id="projectCardImgB">
+                <Image
+                  src={project2}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
               </div>
               <div id="projectCardImg">
-                <img src={blogpic02} />
+                <Image
+                  src={blogpic02}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 col-md-4">
+          <div id="projectPicCard">
+            <Image
+                  src={project1}
+                
+                />
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-4">
+            <div id="projectPicCard">
+            <Image
+                  src={blogpic02}
+                 
+                />
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 col-md-4">
+            <div id="projectCard">
+              <div id="projectCardImgB">
+                <Image
+                  src={blogpic02}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
+              </div>
+              <div id="projectCardImg">
+                <Image
+                  src={project1}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-4">
+            <div id="projectCard">
+              <div id="projectCardImgB">
+                <Image
+                  src={project2}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
+              </div>
+              <div id="projectCardImg">
+                <Image
+                  src={blogpic02}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 col-md-4">
+          <div id="projectPicCard">
+            <Image
+                  src={project1}
+                
+                />
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-4">
+            <div id="projectPicCard">
+            <Image
+                  src={blogpic02}
+                 
+                />
+            </div>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="col-lg-6 col-md-4">
+            <div id="projectCard">
+              <div id="projectCardImgB">
+                <Image
+                  src={blogpic02}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
+              </div>
+              <div id="projectCardImg">
+                <Image
+                  src={project1}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-4">
+            <div id="projectCard">
+              <div id="projectCardImgB">
+                <Image
+                  src={project2}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
+              </div>
+              <div id="projectCardImg">
+                <Image
+                  src={blogpic02}
+                  style={{ borderRadius: 4, height: 150 }}
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
+      <Footer/>
     </>
   );
 }
